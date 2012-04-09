3dc99d3c88cf343cfbfbcf5ad7b88069c036d4ad
Martin Dilger <martin@effectivetrainings.de>
2012-04-09 18:28:33 +0200

    spring powered..

--- ui/src/main/java/de/effective/EffectiveApplication.java
+++ ui/src/main/java/de/effective/EffectiveApplication.java
@@ -4,12 +4,14 @@ import org.apache.wicket.pageStore.memory.IDataStoreEvictionStrategy;
 import org.apache.wicket.pageStore.memory.PageNumberEvictionStrategy;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.serialize.ISerializer;
+import org.springframework.stereotype.Component;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  * 
  * @see de.effective.Start#main(String[])
  */
+@Component
 public class EffectiveApplication extends WebApplication
 {    	
 	/**
